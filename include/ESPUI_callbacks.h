638be0d6aaedecbcb2d68b6ed6b557c9f816e331
Adrian <92983875+adri5104@users.noreply.github.com>
2022-05-06 11:40:09 +0200

    eya

--- include/ESPUI_callbacks.h
+++ include/ESPUI_callbacks.h
@@ -10,36 +10,23 @@
 #include "Config.h"
 #include "TofSensors.h"
 
-
-
 void callback_umbral(Control*, int);
 void callback_tiempo_giro(Control*, int);
 void callback_tiempo_avance(Control*, int);
 void callback_vel_base(Control*, int);
-
-
 void callback_inicio(Control* , int );
 void callback_final(Control* , int );
 void start_callback(Control* , int );
-
 void pad_callback(Control*, int);
 void stop_callback(Control*, int);
-
 void display_mode_callback(Control*, int);
-
 void callback_setkp(Control*, int);
 void callback_setki(Control*, int);
 void callback_setkd(Control*, int);
 void callback_setbutton(Control*, int);
-
 void callback_linebutton(Control*, int);
-
 void callback_start_laberinto(Control*, int);
 void callback_t1_laberinto(Control*, int);
 void callback_t2_laberinto(Control*, int);
-
-
-
-
 
 #endif
